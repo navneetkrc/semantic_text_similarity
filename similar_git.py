408c8f62006f46c477953907684501e7ade73a65
aoshared@in.ibm.com <cloud_user@mopbzp174193.fr.dst.ibm.com>
2020-02-12 06:33:08 -0600

    finalised similarity script, added data files

--- similar_git.py
+++ similar_git.py
@@ -16,28 +16,6 @@ from threading import Timer
 import pdb
 
 
-# ids = np.load(f'ids_gensen.npy')
-# index_faiss=faiss.read_index(f"index_gensen")
-
-ids = np.load(f'ids_gloveee.npy')
-index_faiss=faiss.read_index(f"index_gloveee")
-
-print(index_faiss.is_trained)
-print(index_faiss.ntotal)
-
-
-
-def save_index():
-    global PATH
-    global index_faiss
-    r=faiss.write_index(index_faiss, f"{PATH}/index_faiss_all")
-    
-    t = Timer(86400, save_index)
-    t.start()
-    
-    return True
-
-
 def apply_threshold(row, k=5, threshold=0.95):
     try:
         row=row.to_frame().T
@@ -51,69 +29,80 @@ def apply_threshold(row, k=5, threshold=0.95):
         fname =os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(e, exc_type, fname, exc_tb.tb_lineno)
     return row
-
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-        
-        
-def get_embeddings(texts):
-    vecs = []
-    if len(texts) > 1000:
-        li_splitted = list(chunks(texts, 100))
-
-        for i,grp in enumerate(li_splitted):
-            print(f"{i}/{len(li_splitted)}", end="\r")
-            r=requests.post(embeddings_service, json={"sentences_list": grp})
-            arr = np.asarray(json.loads(r.text)["vectors"])
-            vecs = np.append(vecs, arr)
-        vecs=np.asarray(vecs).reshape(len(texts), 2048)
-    else:
-        r=requests.post(embeddings_service, json={"sentences_list": texts})
-        vecs = np.asarray(json.loads(r.text)["vectors"])
-    return vecs
-
-
-
     
     
-    
-def search_faiss(vec, k=5):
-    print("Here we go !", type(index_faiss), vec.shape)
+def search_faiss(index_faiss, vec, k=5):
     top_k = index_faiss.search(vec, k)
     return list(top_k)
 
-def get_similar_tickets(recs, k=5, threshold=0.95):
-    print("function called")
+
+def create_index(df, ids_col, xb_faiss_col):
     try:
-        print("starting calculations")
-        if "emb" not in recs.columns: 
-            print("getting embeddings: ", recs.shape)
-            
-            texts=recs["text"].tolist()
-            vecs = get_embeddings(texts)
-            recs["emb"]= vecs.tolist()
+        ids=np.array(df[ids_col].str.slice(start=3).tolist()).astype(int)
+        xb_faiss = np.float32(np.asarray(df[xb_faiss_col].tolist()))
+
+        d  = xb_faiss.shape[1] 
+
+        normalize_L2(xb_faiss)
+
+        index_faiss = faiss.IndexIDMap(faiss.IndexFlatIP(d))
+        index_faiss.add_with_ids(xb_faiss, ids)
+
+        print("FAISS index trained: ", index_faiss.is_trained)
+        print("Total db vectors: ", index_faiss.ntotal)
+
+        return index_faiss
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname =os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("Exception get_similar_tickets: ", e, exc_type, fname, exc_tb.tb_lineno)
+        return False
+    
+
+def add_neighbours_text(mydf, train_df):
+    for i in range(1,6):
+        vals=mydf["NN"+str(i)+"_number"].tolist()
+        if pd.isnull(vals).all():
+            mydf["NN"+str(i)+"_text"]=np.nan
         else:
-            print("embedding col already present")
+            mydf["NN"+str(i)+"_text"] = train_df.set_index("key").loc[mydf["NN"+str(i)+"_number"].tolist()].text.tolist()
+    final=mydf[["key", "query_text", "NN1_text","NN1_score", "NN1_number", "NN2_text","NN2_score", "NN2_number", "NN3_text","NN3_score", "NN3_number", "NN4_text","NN4_score", "NN4_number", "NN5_text","NN5_score","NN5_number", ]]
+    final
 
-        xq = np.float32(np.asarray(recs["emb"].tolist()))
-        normalize_L2(xq)
+    return final
 
-        print("query vector created !")
+
+def get_similar_tickets(train_df, test_df, ids_col, xb_faiss_col, k=5, threshold=0.95):
+    print("starting operations")
+    
+    try:
+        if k>5 | k<1:
+            print("currently only functional with k=5")
+            k=5
         
-        result=search_faiss(xq, k)
-        print("Got results from FAISS")
+        print("creating faiss index")
+        index_faiss = create_index(train_df, ids_col, xb_faiss_col)
+        
+        print("starting calculations")
+        if "emb" not in test_df.columns: 
+            print("embeddings not found")
+            return False
+
+        xq = np.float32(np.asarray(test_df["emb"].tolist()))
+        normalize_L2(xq)
+        print("query vector created")
+        
+        result=search_faiss(index_faiss, xq, k)
+        print("got results from FAISS")
         
         nums = lambda t: "KEY"+str(int(t))#.zfill(7)
         vfunc = np.vectorize(nums)
         result[1]=vfunc(result[1])
 
         mydf=pd.DataFrame(np.column_stack(result))
-        mydf["query_key"]=recs["key"].tolist()
-        mydf["query_text"]=recs["text"].tolist()
-        print("create result dataframe")
+        mydf["query_key"]=test_df["key"].tolist()
+        mydf["query_text"]=test_df["text"].tolist()
+        print("creating result dataframe")
         mydf=mydf.rename(columns={
             0: "NN1_score",
             1: "NN2_score",
@@ -132,9 +121,11 @@ def get_similar_tickets(recs, k=5, threshold=0.95):
 
         res = res.assign(is_similar=np.where(pd.isnull(res['NN1_number']) == True , False, True))
 
-        res = pd.merge(recs, res.rename(columns={"query_key": "key"}), on="key", how="inner")
+        res = pd.merge(test_df, res.rename(columns={"query_key": "key"}), on="key", how="inner")
         
-        return res
+        final = add_neighbours_text(res, train_df)
+        
+        return final
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname =os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -143,22 +134,4 @@ def get_similar_tickets(recs, k=5, threshold=0.95):
     
     
 
-    
-# x=datetime.today()
-# y = x.replace(day=x.day, hour=23, minute=0, second=0, microsecond=0) + timedelta(days=1)
-# delta_t=y-x
-
-# secs=delta_t.total_seconds()
-
-# t = Timer(secs, save_index)
-# t.start()
-
-# if __name__=="__main__":
-#     pass
-#     embeddings_service="http://0.0.0.0:5001/get_embeddings"
-
-#     ids = np.load(f'ids_glove.npy')
-#     index_faiss=faiss.read_index(f"index_glove")
-#     print(index_faiss.is_trained)
-#     print(index_faiss.ntotal)
     
